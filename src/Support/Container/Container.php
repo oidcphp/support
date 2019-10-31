b68eb4fcde2d3d415fe7ca8278f49c1f72b72794
MilesChou <jangconan@gmail.com>
2019-10-31 15:55:04 +0800

    Implement the minimal container

---
+++ src/Support/Container/Container.php
@@ -0,0 +1,59 @@
+<?php
+
+declare(strict_types=1);
+
+namespace OpenIDConnect\Support\Container;
+
+use Psr\Container\ContainerInterface;
+
+/**
+ * Minimal implementation for ContainerInterface
+ *
+ * @link https://www.php-fig.org/psr/psr-11/
+ */
+class Container implements ContainerInterface
+{
+    /**
+     * @var array
+     */
+    private $instance;
+
+    /**
+     * @param array $instance
+     */
+    public function __construct(array $instance = [])
+    {
+        $this->instance = $instance;
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function get($id)
+    {
+        if ($this->has($id)) {
+            return $this->instance[$id];
+        }
+
+        throw new EntryNotFoundException("The entry '{$id}' is not found");
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function has($id)
+    {
+        return isset($this->instance[$id]);
+    }
+
+    /**
+     * Inject instance
+     *
+     * @param string $id
+     * @param mixed $instance
+     */
+    public function set(string $id, $instance): void
+    {
+        $this->instance[$id] = $instance;
+    }
+}
---
+++ tests/Support/Container/ContainerTest.php
@@ -0,0 +1,49 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tests\Support\Container;
+
+use OpenIDConnect\Support\Container\Container;
+use OpenIDConnect\Support\Http\Query;
+use PHPUnit\Framework\TestCase;
+use Psr\Container\NotFoundExceptionInterface;
+
+class ContainerTest extends TestCase
+{
+    /**
+     * @test
+     */
+    public function shouldSetAndGetTheEntry(): void
+    {
+        $target = new Container();
+
+        $target->set('some-entry', 'some-instance');
+
+        $this->assertSame('some-instance', $target->get('some-entry'));
+    }
+
+    /**
+     * @test
+     */
+    public function shouldGetEntryWithPrepareEntry(): void
+    {
+        $target = new Container([
+            'some-entry' => 'some-instance',
+        ]);
+
+        $this->assertSame('some-instance', $target->get('some-entry'));
+    }
+
+    /**
+     * @test
+     */
+    public function shouldThrowExceptionWhenEntryNotFound(): void
+    {
+        $this->expectException(NotFoundExceptionInterface::class);
+
+        $target = new Container();
+
+        $target->get('not-found-entry');
+    }
+}
